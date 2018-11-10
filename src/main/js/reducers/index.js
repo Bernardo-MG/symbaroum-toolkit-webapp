28a52066d4484c8f0e1940dbe60f449a4dbd710d
Bernardo <programming@wandrell.com>
2018-11-10 21:40:53 +0100

    Removed sample reducers

--- src/main/js/reducers/index.js
+++ src/main/js/reducers/index.js
@@ -1,18 +1,10 @@
 import { combineReducers } from 'redux';
 import { routerReducer as routing } from 'react-router-redux';
 import views from 'views/reducers';
-import { entities } from 'example/reducers/pagination';
 import chargen from 'chargen/reducers';
-import model from 'example/reducers';
-
-const pagination = combineReducers({
-   entities
-});
 
 const rootReducer = combineReducers({
-   pagination,
    chargen,
-   model,
    routing,
    views
 });
